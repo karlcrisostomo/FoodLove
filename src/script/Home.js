cf9acb12233f03f6621d606263d27360828ad46d
KarlCrisostomo12 <101928489+KarlCrisostomo12@users.noreply.github.com>
2023-06-13 15:53:43 +0800

    base

--- src/script/Home.js
+++ src/script/Home.js
@@ -1,10 +1,24 @@
-import { ref, computed } from "vue";
+import { ref, computed, } from "vue";
 import { useRoute } from "vue-router";
 
 export default {
   setup() {
     const isMenuOpen = ref(false);
     const route = useRoute();
+    const menuContainer = ref(null);
+
+    const handleClickOutside = (event) => {
+      if (menuContainer.value && !menuContainer.value.contains(event.target)) {
+        // Close the menu by resetting any relevant state or class
+        // For example, you could use a reactive variable like `isOpen` to control the menu's visibility
+        // isOpen.value = false;
+        // Or remove a CSS class
+        // menuContainer.value.classList.remove('navCon-open');
+      }
+    };
+
+  
+
     const hideDiv = computed(() => {
       return route.name === "error";
     });
@@ -17,6 +31,8 @@ export default {
       isMenuOpen,
       toggleMenu,
       hideDiv,
+      menuContainer,
+      handleClickOutside,
     };
   },
 };
