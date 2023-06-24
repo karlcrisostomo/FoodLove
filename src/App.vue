2e3a8de5b50d9cc065f8ca3f8755249478942985
KarlCrisostomo12 <101928489+KarlCrisostomo12@users.noreply.github.com>
2023-06-24 19:11:15 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 
 <template>
   <nav class="navbar">
-    <span @click="$router.push('/')"  class="title">FOODLOVE</span>
+    <span @click="$router.push('/')"  class="title">Cuisine Explorer</span>
 
     <div :class="['navCon', { 'navCon-open': isMenuOpen }]">
       <div class="hamburger" @click="toggleMenu">
